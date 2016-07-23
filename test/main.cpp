e93c1433af2fd7787d67c6f0e4a899d8e69f2872
stanthomas <stanthomas@users.noreply.github.com>
2016-07-23 01:32:01 +0100

    Create main.cpp

---
+++ test/main.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <iostream>
+#include <conio.h>
+
+#include "../tixml2ex.h"
+
+using namespace std;
+using namespace std::literals::string_literals;
+
+
+int main()
+{
+  // coming soon ...
+
+  return 0;
+}
